78ef3586a256ec34d5ba132fc30ada97d3b42136
Thomas Ruschival <thomas@ruschival.de>
2024-12-18 22:16:21 +0100

    First CLI
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+pub mod botcommands {
+
+    #[derive(Debug)]
+    pub enum CliError {
+        CommandUnknown,
+        CommandNotImplemented,
+    }
+
+    pub fn command_for_string(cmd: &str) -> Result<impl Command, CliError> {
+        let cmd = cmd.to_uppercase();
+        match cmd.trim() {
+            "NORTH" => Ok(GoSouth::new()),
+            "SOUTH" => Ok(GoSouth::new()),
+            "WEST" | "East" => Err(CliError::CommandNotImplemented),
+            "NEAR" | "FAR" => Err(CliError::CommandNotImplemented),
+            _ => Err(CliError::CommandUnknown),
+        }
+    }
+
+    pub trait Command {
+        fn execute(&self);
+        fn info(&self);
+    }
+
+    pub struct GoNorth {
+        info: String,
+    }
+
+    impl Default for GoNorth {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl GoNorth {
+        pub fn new() -> Self {
+            Self {
+                info: "Going North!".to_string(),
+            }
+        }
+    }
+
+    impl Command for GoNorth {
+        fn execute(&self) {
+            self.info();
+        }
+        fn info(&self) {
+            println!("{}", self.info);
+        }
+    }
+
+    // Going South
+    #[derive(Debug)]
+    pub struct GoSouth {
+        pub info: String,
+    }
+
+    impl Command for GoSouth {
+        fn execute(&self) {
+            self.info();
+        }
+        fn info(&self) {
+            println!("{}", self.info);
+        }
+    }
+
+    impl Default for GoSouth {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl GoSouth {
+        pub fn new() -> Self {
+            Self {
+                info: "Going South".to_string(),
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use rustyline::config::Configurer;
+use rustyline::error::ReadlineError;
+use rustyline::{Cmd, DefaultEditor, KeyEvent};
+use warehousebot::botcommands;
+use warehousebot::botcommands::Command;
+
+fn main() -> rustyline::Result<()> {
+    let mut rl = DefaultEditor::new()?;
+    rl.set_edit_mode(rustyline::EditMode::Emacs);
+    rl.bind_sequence(KeyEvent::ctrl('s'), Cmd::HistorySearchForward);
+    rl.bind_sequence(KeyEvent::ctrl('r'), Cmd::HistorySearchBackward);
+    #[cfg(feature = "with-file-history")]
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str())?;
+                println!("Line: {}", line);
+
+                match botcommands::command_for_string(&line) {
+                    Ok(cmd) => {
+                        println!("ok, executing {:?}", cmd.info());
+                        cmd.execute()
+                    }
+                    Err(e) => {
+                        println!("Bad Error {:?}", e);
+                    }
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    #[cfg(feature = "with-file-history")]
+    rl.save_history("history.txt")?;
+    Ok(())
+}
